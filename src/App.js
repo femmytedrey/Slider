537427408e123a27deea441f14b38c6ed0701409
femmytedrey <femmytedrey@gmail.com>
2023-12-16 23:55:59 +0100

    in progress

--- src/App.js
+++ src/App.js
@@ -11,6 +11,31 @@ function App() {
         <span>/</span>reviews
       </h2>
     </div>
+    <div className='section-center'>
+      {
+        people.map((person, personIndex) => {
+          const {id, image, name, title, quote} = person;
+          //more coming up soon
+          let position = 'nextSlide'
+          if(personIndex === index){
+            position = 'activeSlide'
+          }
+          return <article className={position} key={id}>
+            <img src={image} alt = {name} className='person-img' />
+            <h4>{name}</h4>
+            <p className='title'>{title}</p>
+            <p className='text'>{quote}</p>
+            <FaQuoteRight className='icon' />
+          </article>
+        })
+      }
+      <button className='prev'>
+        <FiChevronLeft />
+      </button>
+      <button className='next'>
+        <FiChevronRight />
+      </button>
+    </div>
   </section>;
 }
 
